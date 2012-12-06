ecffb1c17ab639b05c8e8af12a06f9e5e7b55613
David Fisher <tibbon@gmail.com>
2012-12-06 15:06:08 -0500

    Generated new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsolrTest::Application.config.secret_token = '16b4a92e8e5e4f435f8e3eb5ef4b9e0f0a40a18d1d2fe9f9d21a2e900b81253eceb6ea1d69033f66a5a45e9af4b222bdb92e03583f17b87e21edcb18eda2b6bb'
